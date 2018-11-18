0df2c7f008b17cd12b910d25160495654573f60d
stobita <tobita.esc@gmail.com>
2018-11-18 18:20:50 +0900

    認証処理の基礎を作成

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,15 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(["middleware" => "guest:api"], function () {
+    Route::post("/hello", "AuthController@hello");
+    Route::post("/signup", "AuthController@signup");
+    Route::post("/login", "AuthController@login");
+});
+
+Route::group([ 'middleware' => 'auth:api', ], function ($router) {
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+});
