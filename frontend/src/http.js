288c31fe1ecdfcd671595643ee869e3c2908ddc2
stobita <tobita.esc@gmail.com>
2018-11-20 21:21:11 +0900

    APIリクエスト時のtoken付与

--- frontend/src/http.js
+++ frontend/src/http.js
@@ -3,5 +3,9 @@ import axios from "axios";
 const http = axios.create();
 
 http.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+http.interceptors.request.use(config => {
+  config.headers.Authorization = `Bearer ${localStorage.getItem("jwt")}`;
+  return config;
+});
 
 export default http;
